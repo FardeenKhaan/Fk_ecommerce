64bf2656bb3f5c71afe1ee6227de8eb5fd2e3eb1
Fardeen Khan <fardeenkhaan70@gmail.com>
2025-08-04 17:48:18 +0500

    more cleaner code

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
-import 'package:fk_ecommerce/features/authentication/screens/OnBoarding/onboarding_screen.dart';
 import 'package:fk_ecommerce/routes/app_pages.dart';
 import 'package:fk_ecommerce/routes/app_routes.dart';
-import 'package:fk_ecommerce/utils/constants/colors.dart';
 import 'package:fk_ecommerce/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 void main() {
   // FKColors.linearGradient;
+
   runApp(const MyApp());
 }
 
