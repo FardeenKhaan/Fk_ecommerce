eb437a249724162ebcc530b043b7dc9ebcdfee54
Fardeen Khan <fardeenkhaan70@gmail.com>
2025-08-09 23:02:27 +0500

    continue works on store screen

--- lib/features/authentication/screens/store/store.dart
+++ lib/features/authentication/screens/store/store.dart
@@ -1,7 +1,10 @@
 import 'package:fk_ecommerce/common/widgets/appbar/appbar.dart';
+import 'package:fk_ecommerce/common/widgets/custom_shapes/containers/rounded_container.dart';
 import 'package:fk_ecommerce/common/widgets/custom_shapes/containers/search_container.dart';
 import 'package:fk_ecommerce/common/widgets/products/cart/cart_menu_icon.dart';
+import 'package:fk_ecommerce/common/widgets/texts/section_heading.dart';
 import 'package:fk_ecommerce/utils/constants/colors.dart';
+import 'package:fk_ecommerce/utils/constants/image_Strings.dart';
 import 'package:fk_ecommerce/utils/constants/sizes.dart';
 import 'package:fk_ecommerce/utils/helpers/helper_function.dart';
 import 'package:flutter/material.dart';
@@ -39,8 +42,24 @@ class FkStoreScreen extends StatelessWidget {
                       text: 'Search in Store',
                       showBorder: true,
                       showBackground: false,
+                      padding: EdgeInsets.zero,
                     ),
                     SizedBox(height: FKSizes.spaceBtwItems),
+
+                    // --Featured brands
+                    FkSectionHeading(
+                      title: 'Features Brands',
+
+                      onPressed: () {},
+                    ),
+                    SizedBox(height: FKSizes.spaceBtwItems / 1.5),
+
+                    FkRoundedContainer(
+                      padding: EdgeInsets.all(FKSizes.sm),
+                      showBorder: true,
+                      backgroundColor: Colors.transparent,
+                      child: Row(children: [FkCircularImage()]),
+                    ),
                   ],
                 ),
               ),
@@ -48,6 +67,28 @@ class FkStoreScreen extends StatelessWidget {
           ];
         },
         body: Container(),
+      ),
+    );
+  }
+}
+
+class FkCircularImage extends StatelessWidget {
+  const FkCircularImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = FkHelperFunction.isDarkMode(context);
+    return Container(
+      width: 56,
+      height: 56,
+      padding: EdgeInsets.all(FKSizes.sm),
+      decoration: BoxDecoration(
+        color: dark ? FKColors.black : FKColors.white,
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: Image(
+        image: AssetImage(FKImageStrings.clothIcon),
+        color: dark ? FKColors.white : FKColors.dark,
       ),
     );
   }
