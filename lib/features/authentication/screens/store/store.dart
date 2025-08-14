09ecfaa44a0d132dfc4fee7b405d6636eb35d081
Fardeen Khan <fardeenkhaan70@gmail.com>
2025-08-15 00:16:59 +0500

    update the code of store screen

--- lib/features/authentication/screens/store/store.dart
+++ lib/features/authentication/screens/store/store.dart
@@ -1,9 +1,13 @@
 import 'package:fk_ecommerce/common/widgets/appbar/appbar.dart';
 import 'package:fk_ecommerce/common/widgets/custom_shapes/containers/rounded_container.dart';
 import 'package:fk_ecommerce/common/widgets/custom_shapes/containers/search_container.dart';
+import 'package:fk_ecommerce/common/widgets/images/fk_circular_image.dart';
+import 'package:fk_ecommerce/common/widgets/layouts/grid_layout.dart';
 import 'package:fk_ecommerce/common/widgets/products/cart/cart_menu_icon.dart';
+import 'package:fk_ecommerce/common/widgets/texts/fk_brand_title_text_with_verified_icon.dart';
 import 'package:fk_ecommerce/common/widgets/texts/section_heading.dart';
 import 'package:fk_ecommerce/utils/constants/colors.dart';
+import 'package:fk_ecommerce/utils/constants/enums.dart';
 import 'package:fk_ecommerce/utils/constants/image_Strings.dart';
 import 'package:fk_ecommerce/utils/constants/sizes.dart';
 import 'package:fk_ecommerce/utils/helpers/helper_function.dart';
@@ -14,6 +18,7 @@ class FkStoreScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dark = FkHelperFunction.isDarkMode(context);
     return Scaffold(
       appBar: FkAppBar(
         title: Text('Store', style: Theme.of(context).textTheme.headlineMedium),
@@ -26,10 +31,7 @@ class FkStoreScreen extends StatelessWidget {
               automaticallyImplyLeading: false,
               pinned: true,
               floating: true,
-              backgroundColor:
-                  FkHelperFunction.isDarkMode(context)
-                      ? FKColors.black
-                      : FKColors.white,
+              backgroundColor: dark ? FKColors.black : FKColors.white,
               expandedHeight: 440,
               flexibleSpace: Padding(
                 padding: EdgeInsets.all(FKSizes.defaultSpace),
@@ -53,12 +55,53 @@ class FkStoreScreen extends StatelessWidget {
                       onPressed: () {},
                     ),
                     SizedBox(height: FKSizes.spaceBtwItems / 1.5),
+                    FkGridLayout(
+                      itemcount: 4,
+                      mainAxisExtent: 80,
+                      itembuilder: (_, index) {
+                        return GestureDetector(
+                          onTap: () {},
+                          child: FkRoundedContainer(
+                            padding: EdgeInsets.all(FKSizes.sm),
+                            showBorder: true,
+                            backgroundColor: Colors.transparent,
 
-                    FkRoundedContainer(
-                      padding: EdgeInsets.all(FKSizes.sm),
-                      showBorder: true,
-                      backgroundColor: Colors.transparent,
-                      child: Row(children: [FkCircularImage()]),
+                            child: Row(
+                              children: [
+                                Flexible(
+                                  child: FkCircularImage(
+                                    image: FKImageStrings.clothIcon,
+                                    overlayColor:
+                                        dark ? FKColors.white : FKColors.black,
+                                  ),
+                                ),
+                                SizedBox(width: FKSizes.spaceBtwItems / 2),
+
+                                /// -- Text
+                                Expanded(
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      FkBrandTitleWithVerifiedIcon(
+                                        title: 'Nike',
+                                        brandTextSizes: TextSizes.large,
+                                      ),
+                                      Text(
+                                        '256 products',
+                                        overflow: TextOverflow.ellipsis,
+                                        style:
+                                            Theme.of(
+                                              context,
+                                            ).textTheme.labelMedium,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
                     ),
                   ],
                 ),
@@ -67,28 +110,6 @@ class FkStoreScreen extends StatelessWidget {
           ];
         },
         body: Container(),
-      ),
-    );
-  }
-}
-
-class FkCircularImage extends StatelessWidget {
-  const FkCircularImage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final dark = FkHelperFunction.isDarkMode(context);
-    return Container(
-      width: 56,
-      height: 56,
-      padding: EdgeInsets.all(FKSizes.sm),
-      decoration: BoxDecoration(
-        color: dark ? FKColors.black : FKColors.white,
-        borderRadius: BorderRadius.circular(100),
-      ),
-      child: Image(
-        image: AssetImage(FKImageStrings.clothIcon),
-        color: dark ? FKColors.white : FKColors.dark,
       ),
     );
   }
