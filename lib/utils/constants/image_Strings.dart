9e6aecab0e623ba78ed6f1722ed9084adbc640ce
Fardeen Khan <fardeenkhaan70@gmail.com>
2025-08-18 19:01:01 +0500

    update the images path

--- lib/utils/constants/image_Strings.dart
+++ lib/utils/constants/image_Strings.dart
@@ -10,42 +10,27 @@ class FKImageStrings {
   static const String facebook = "assets/logos/facebook.png";
 
   // -- OnBoarding Images
-  static const String onBoardingImage1 =
-      "assets/images/on_boarding_images/man.png";
-  static const String onBoardingImage2 =
-      "assets/images/on_boarding_images/boy.png";
-  static const String onBoardingImage3 =
-      "assets/images/on_boarding_images/reading.png";
+  static const String onBoardingImage1 = "assets/images/on_boarding_images/man.png";
+  static const String onBoardingImage2 = "assets/images/on_boarding_images/boy.png";
+  static const String onBoardingImage3 = "assets/images/on_boarding_images/reading.png";
 
   // -- Animations
-  static const String verifyIllustration =
-      "assets/images/animations/verify_illustration.png";
-  static const String productsSoleIllustration =
-      "assets/images/animations/sammy-line-sale.png";
-  static const String staticSuccessIllustration =
-      "assets/images/animations/sammy-line-success.png";
-  static const String deliveredInPioneIllustration =
-      "assets/images/animations/sammy-line-come-back-later.png";
-  static const String deliveredEmailIllustration =
-      "assets/images/animations/sammy-line-man-receives-a-mail.png";
+  static const String verifyIllustration = "assets/images/animations/verify_illustration.png";
+  static const String productsSoleIllustration = "assets/images/animations/sammy-line-sale.png";
+  static const String staticSuccessIllustration = "assets/images/animations/sammy-line-success.png";
+  static const String deliveredInPioneIllustration = "assets/images/animations/sammy-line-come-back-later.png";
+  static const String deliveredEmailIllustration = "assets/images/animations/sammy-line-man-receives-a-mail.png";
   // static const String verifyIllustration = "assets/images/animations/sammy-line-travel-backpack-with-passport-and-air-ticket.gif";
 
   // -- Category Icons
-  static const String sportIcon =
-      "assets/icons/categories/icons8-bowling-48.png";
+  static const String sportIcon = "assets/icons/categories/icons8-bowling-48.png";
   static const String shoeIcon = "assets/icons/categories/icons8-shoes-48.png";
-  static const String cosmeticIcon =
-      "assets/icons/categories/icons8-cosmetics-48.png";
-  static const String animalIcon =
-      "assets/icons/categories/icons8-dog-heart-64.png";
-  static const String toyIcon =
-      "assets/icons/categories/icons8-wooden-toy-car-50.png";
-  static const String furnitureIcon =
-      "assets/icons/categories/icons8-dining-chair-64.png";
-  static const String jeweleryIcon =
-      "assets/icons/categories/icons8-sparkling-diamond-64.png";
-  static const String electronicsIcon =
-      "assets/icons/categories/icons8-smartphone-48.png";
+  static const String cosmeticIcon = "assets/icons/categories/icons8-cosmetics-48.png";
+  static const String animalIcon = "assets/icons/categories/icons8-dog-heart-64.png";
+  static const String toyIcon = "assets/icons/categories/icons8-wooden-toy-car-50.png";
+  static const String furnitureIcon = "assets/icons/categories/icons8-dining-chair-64.png";
+  static const String jeweleryIcon = "assets/icons/categories/icons8-sparkling-diamond-64.png";
+  static const String electronicsIcon = "assets/icons/categories/icons8-smartphone-48.png";
   static const String clothIcon = "assets/icons/categories/cloth_icon.png";
 
   // Banners
@@ -56,16 +41,11 @@ class FKImageStrings {
   // -- Products
   static const String productImages = "assets/images/products/nike-shoes.png";
   static const String productImages2 = "assets/images/products/product-1.png";
-  static const String productImages3 =
-      "assets/images/products/product-jacket.png";
-  static const String productImages4 =
-      "assets/images/products/product-jeans.png";
-  static const String productImages5 =
-      "assets/images/products/product-shirt.png";
-  static const String productImages6 =
-      "assets/images/products/product-slippers.png";
-  static const String productImages7 =
-      "assets/images/products/nikeairjordanbackhat.png";
+  static const String productImages3 = "assets/images/products/product-jacket.jpg";
+  static const String productImages4 = "assets/images/products/product-jeans.png";
+  static const String productImages5 = "assets/images/products/product-shirt.png";
+  static const String productImages6 = "assets/images/products/product-slippers.jpg";
+  static const String productImages7 = "assets/images/products/nikeairjordanbackhat.png";
   //   static const String productImages8 = "assets/images/products/nikeairjordanorange.png";
   //   static const String productImages9 = "assets/images/products/nikeairjordanwhitepants.png";
   //   static const String productImages10 = "assets/images/products/nikeairjordanwhiteback.png";
@@ -89,10 +69,7 @@ class FKImageStrings {
   //   static const String productImages28 = "assets/images/products/adidas_football_bag.png";
   //   static const String productImages29 = "assets/images/products/triple_bat_set.png";
   //   static const String productImages30 = "assets/images/products/adidas_bucket_hat.png";
-  static const String productImages31 =
-      "assets/images/products/adidas_red_bag.png";
-  static const String productImages32 =
-      "assets/images/products/adidas_pink_bag.png";
-  static const String productImages33 =
-      "assets/images/products/adidas_brown_bag.png";
+  static const String productImages31 = "assets/images/products/adidas_red_bag.png";
+  static const String productImages32 = "assets/images/products/adidas_pink_bag.png";
+  static const String productImages33 = "assets/images/products/adidas_brown_bag.png";
 }
